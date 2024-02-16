f03c63f7a670b63f258df825255926e35371d0f5
S.Tougeiro <sidneytougeiro@gmail.com>
2024-02-16 19:53:00 -0300

    first commit

---
+++ src/CacheHandlerInterface.php
@@ -0,0 +1,9 @@
+<?php declare(strict_types=1);
+
+    namespace STDW\Contract\Cache;
+
+    use Psr\SimpleCache\CacheInterface as PsrCacheInterface;
+
+
+    interface CacheHandlerInterface extends PsrCacheInterface
+    { }
